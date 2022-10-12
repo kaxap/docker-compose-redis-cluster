2098c573e3ccf2747d4455b5564ff007e389ffb6
kaxap <2851970+kaxap@users.noreply.github.com>
2022-10-12 12:45:46 +0200

    Update Redis version to 6.2

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 
 docker-compose up -d
 docker network ls
-docker network inspect [docker-compose-redis-cluster_redis-cluster id]
+docker network inspect docker-compose-redis-cluster_redis-cluster
 docker exec -it redis-cluster-5 redis-cli --cluster create 172.21.0.2:6380 172.21.0.3:6381 172.21.0.4:6382 172.21.0.5:6383 172.21.0.6:6384 172.21.0.7:6385 --cluster-replicas 1 -a Szzf@2022
+
